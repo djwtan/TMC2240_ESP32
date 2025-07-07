5985118674c730fd5ad38d4cc61a3cc2d9d9bba0
jwtan5 <55527138+jwtan5@users.noreply.github.com>
2025-07-07 23:08:04 +0100

    tmp

---
+++ src/board/config.h
@@ -0,0 +1,9 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+    #define STEPPER_1
+    #define STEPPER_2
+    #define STEPPER_3
+    #define STEPPER_4
+
+#endif
---
+++ src/board/init..h
@@ -0,0 +1,13 @@
+#ifndef INIT_H
+#define INIT_H
+
+    #ifdef STEPPER_1
+    #endif
+    #ifdef STEPPER_2
+    #endif
+    #ifdef STEPPER_3
+    #endif
+    #ifdef STEPPER_4
+    #endif
+
+#endif
